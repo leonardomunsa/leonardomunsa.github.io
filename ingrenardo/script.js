d78b85a5d675476eaad4e8312633166aba8967a2
Leonardo <leo_mc_inter@hotmail.com>
2021-09-23 20:08:18 -0300

    Falta um tempao slk

---
+++ ingrenardo/script.js
@@ -0,0 +1,13 @@
+let finalContrato = new Date("Feb 17, 2022 23:59:59").getTime();
+
+let countDown = setInterval(function() {
+    let agora = new Date().getTime();
+    let tempoRestante = finalContrato - agora;
+
+    let dias = Math.floor(tempoRestante / (1000 * 60 * 60 * 24));
+    let horas = Math.floor((tempoRestante % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutos = Math.floor((tempoRestante % (1000 * 60 * 60)) / (1000 * 60));
+    let segundos = Math.floor((tempoRestante % (1000 * 60)) / 1000);
+
+    document.getElementById('contador').innerHTML = dias + ' dias, ' + horas + ' horas, ' + minutos + ' minutos, ' + segundos + ' segundos';
+}, 1000)
